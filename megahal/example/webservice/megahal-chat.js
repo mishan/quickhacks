7044487ec8681aa44ea416694bab638384e8e6d3
Misha Nasledov <misha@nasledov.com>
2013-11-30 22:32:32 -0800

    Add comments

--- megahal/example/webservice/megahal-chat.js
+++ megahal/example/webservice/megahal-chat.js
@@ -6,7 +6,6 @@ function getUserPrefix() {
     return '<font color="blue">User: </font>';
 }
 
-// jQuery Document
 $(document).ready(function() {
     // get greeting
     $.get('/~misha/megahal/talk.php', {}, function(res) {
@@ -16,25 +15,32 @@ $(document).ready(function() {
         }
     });
 
+    // handle user input
     $('.input-form').submit(function(e) {
         var userMsgBox = $('.user-input');
         var clientMsg = userMsgBox.val();
         var chatbox = $('.megahal-chatbox');
 
+        // append user input
         chatbox.append(getUserPrefix(''));
+        // escape HTML
         $(document.createTextNode(clientMsg)).appendTo(chatbox);
         chatbox.append('<br/><br/>');
+        // reset input
         userMsgBox.val('');
+        // query bot
         $.get('/~misha/megahal/talk.php?q='+encodeURIComponent(clientMsg), {}, function(res) {
             if (res) {
                 var resObj = $.parseJSON(res);
                 chatbox.append(getHALText(resObj.reply));
             }
+            // scroll to bottom
             chatbox.animate({
                 scrollTop: chatbox.height()
             }, 300);
         });
 
+        // don't do form submit
         return false;
     });
 });
